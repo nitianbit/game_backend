454c359de4a4dbbeb70cb8184fc1d6924a7058e4
Tushar <tushar.mangla1120@gmail.com>
2024-04-20 03:24:31 +0530

    add admin login

--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getProfile, login, signup } from './controller.js';
+import { getProfile, login, signup, adminLogin } from './controller.js';
 const authRouter = express.Router();
 
 authRouter.post("/login", login)
+authRouter.post("/adminLogin", adminLogin)
 authRouter.post("/signup", signup)
 
 authRouter.get("/get", getProfile)
