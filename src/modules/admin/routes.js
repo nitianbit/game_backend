902f7f63ba5ef4cd20e8fc3e21335a392eb2df86
Tushar <tushar.mangla1120@gmail.com>
2024-04-20 03:23:12 +0530

    changes

--- src/modules/admin/routes.js
+++ src/modules/admin/routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { getUsers, updateUser, deleteUser } from './controller.js';
+import { getUsers, updateSingleUser, deleteSingleUser, getSingleUser } from './controller.js';
 const adminRouter = express.Router();
 
-adminRouter.post("/updateUser/:userId", updateUser)
-adminRouter.post("/deleteUser/:userId", deleteUser)
+adminRouter.post("/updateUser/:userId", updateSingleUser)
+adminRouter.post("/deleteUser/:userId", deleteSingleUser)
 
 adminRouter.get("/users-grid", getUsers)
-adminRouter.get("/user/:userId", getUsers)
+adminRouter.get("/user/:userId", getSingleUser)
 
 export default adminRouter;
 
