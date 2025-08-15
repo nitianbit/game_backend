488b9fbdbfbff4fbcfa88eb9929d59c99b39e2ae
Your Name <nitianbit@gmail.com>
2025-08-15 20:36:26 +0530

    addmoney , deductMoney, transferMoney api added

--- src/modules/payment/routes.js
+++ src/modules/payment/routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { checkPaymentDone, checkPaymentStatus, initiatePayment, markPaymentDone } from './controller.js';
+import { addMoney, checkPaymentDone, checkPaymentStatus, DeductMoney, initiatePayment, markPaymentDone, transferPayment } from './controller.js';
 const paymentRouter = express.Router();
 
 paymentRouter.post('/initiate', initiatePayment);
 paymentRouter.get('/status', checkPaymentStatus);
 paymentRouter.post('/mark-capture', markPaymentDone);
+paymentRouter.post('paymentTransfer',transferPayment);
+paymentRouter.post('deductMoney',DeductMoney);
+paymentRouter.post('addMoney',addMoney);
 //TODO add scheduler
 paymentRouter.post('/payment-scheduler', checkPaymentDone);
 
